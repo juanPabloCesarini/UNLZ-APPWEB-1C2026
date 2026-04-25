f7bde259057200de50620e5315352c7df2690cb2
Coronel Cristian <cristiandaniiel3@gmail.com>
2026-04-25 13:45:28 -0300

    feat calculadora

---
+++ CoronelCristian/tp-calculadora/index.php
@@ -0,0 +1,35 @@
+<?php
+    $titulo = "Clase 25-04-26 PHP";
+    include_once 'View/layout/header.php';
+?>
+
+    <div class="container-fluid bg-dark m-3 p-3">
+        <div class="text-center text-white p-2 m-3">
+            <h1>CALCULADORA</h1>
+        </div>
+        <div class="card shadow bg-secondary text-white text-center text-uppercase">
+            <p class="card-title">Ingresá los numeros a calcular</p>
+            <div class="card-body">
+                <div class="mb-3">
+                    <form action="Controller/calcular.php" method="post">
+
+                        <label for="num1" class="form-label">Primer Número</label>
+                        <input type="number" class="form-control" id="num1" name="num1">
+                </div>
+                <div class="mb-3">
+                    <label for="num2" class="form-label">Segundo Número</label>
+                    <input type="number" class="form-control" id="num2" name="num2">
+                </div>
+       <select class="form-select" aria-label="Default select example" name="operacion">
+  <option selected>Elegir..</option>
+  <option value="+">Sumar</option>
+  <option value="-">Restar</option>
+  <option value="*">Multiplicar</option>
+  <option value="/">dividir</option>
+</select>
+                <button class="btn btn-block btn-primary" type="submit">CALCULAR</button>
+                </form>
+            </div>
+        </div>
+    </div>
+<?php include_once 'View/layout/footer.php';?>
