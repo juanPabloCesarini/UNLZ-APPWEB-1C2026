ddd5bea0c09a3468bab8bf4fc1f568b607cde776
Lucia Raising <luraising@outlook.com>
2026-04-26 21:50:36 -0300

    Agregar TP

---
+++ RaisingLucia/Tp-Calculadora/view/vista_resultado.php
@@ -0,0 +1,19 @@
+<?php require_once("header.php"); ?>
+
+<div class="container mt-5">
+    <div class="card p-4 shadow text-center">
+
+        <h2>Resultado</h2>
+
+        <h1 class="mt-4">
+            <?php echo $resultado; ?>
+        </h1>
+
+        <a href="/RaisingLucia/Tp-Calculadora/index.php" class="btn btn-primary mt-4">
+            Volver
+        </a>
+
+    </div>
+</div>
+
+<?php require_once("footer.php"); ?>
