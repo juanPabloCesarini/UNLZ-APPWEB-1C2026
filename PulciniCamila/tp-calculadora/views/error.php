4a927b81e0a0e39e71fddbb713e263c02b64507f
Camila <pulcinicamila@gmail.com>
2026-04-29 20:07:02 -0300

    TP calculadora terminado

---
+++ PulciniCamila/tp-calculadora/views/error.php
@@ -0,0 +1,40 @@
+<?php
+$titulo = "Error";
+include_once 'layout/header.php';
+?>
+
+<main>
+    <div class="container">
+        <div class="calculadora">
+
+            <h1>Calculadora</h1>
+
+            <form action="../controller/calcular.php" method="post">
+
+                <input type="number" name="num1" placeholder="Primer número">
+                <input type="number" name="num2" placeholder="Segundo número">
+
+                <br>
+
+                <select name="operacion">
+                    <option value="+">Sumar</option>
+                    <option value="-">Restar</option>
+                    <option value="*">Multiplicar</option>
+                    <option value="/">Dividir</option>
+                </select>
+
+                <br><br>
+
+                <button type="submit">Calcular</button>
+
+            </form>
+
+            <div class="error">
+                <p><?php echo $errorMsg; ?></p>
+            </div>
+
+        </div>
+    </div>
+</main>
+
+<?php include_once 'layout/footer.php'; ?>
