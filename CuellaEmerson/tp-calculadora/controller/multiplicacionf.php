b4b534649ec0de25db1830eb37ba76b1b117f878
unknown <you@example.com>
2026-04-30 00:26:04 -0300

    feat calcculadora

---
+++ CuellaEmerson/tp-calculadora/controller/multiplicacionf.php
@@ -0,0 +1,11 @@
+<?php
+
+$numero1 = $_POST['num1'];
+$numero2 = $_POST['num2'];
+$opera= $_POST['operacion'];
+
+$resultado = $numero1*$numero2;
+
+require_once '../view/vista_resultado.php';
+
+?>
